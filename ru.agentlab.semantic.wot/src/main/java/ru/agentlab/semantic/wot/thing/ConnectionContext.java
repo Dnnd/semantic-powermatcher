0ebe805d0ac78466e90a734ef0cea98474bcd141
Dnnd <d.v.maslennikov@outlook.com>
2021-06-17 16:12:18 +0300

    Add Sail type discovery to ConnectionContext
    From now ConnectionContext provides SailConnection to Sail specified in constructor

--- ru.agentlab.semantic.wot/src/main/java/ru/agentlab/semantic/wot/thing/ConnectionContext.java
+++ ru.agentlab.semantic.wot/src/main/java/ru/agentlab/semantic/wot/thing/ConnectionContext.java
@@ -1,7 +1,10 @@
 package ru.agentlab.semantic.wot.thing;
 
+import org.eclipse.rdf4j.repository.sail.SailRepository;
 import org.eclipse.rdf4j.repository.sail.SailRepositoryConnection;
+import org.eclipse.rdf4j.sail.Sail;
 import org.eclipse.rdf4j.sail.SailConnection;
+import org.eclipse.rdf4j.sail.helpers.SailUtil;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
@@ -13,11 +16,23 @@ public class ConnectionContext {
 
     private final Scheduler scheduler;
     private final SailRepositoryConnection connection;
+    private final SailConnection sailConnection;
 
-    public ConnectionContext(ExecutorService executor, SailRepositoryConnection connection) {
+    public ConnectionContext(ExecutorService executor,
+                             SailRepositoryConnection connection,
+                             SailConnection sailConnection) {
         this.executor = executor;
-        this.connection = connection;
         this.scheduler = Schedulers.fromExecutor(executor);
+        this.connection = connection;
+        this.sailConnection = sailConnection;
+    }
+
+    public <T extends Sail> ConnectionContext(ExecutorService executor, SailRepository repository, Class<T> sailClass) {
+        this.executor = executor;
+        this.scheduler = Schedulers.fromExecutor(executor);
+        var sail = SailUtil.findSailInStack(repository.getSail(), sailClass);
+        this.sailConnection = sail.getConnection();
+        this.connection = repository.getConnection();
     }
 
     public void close() {
@@ -34,7 +49,7 @@ public class ConnectionContext {
     }
 
     public SailConnection getSailConnection() {
-        return connection.getSailConnection();
+        return sailConnection;
     }
 
     public ExecutorService getExecutor() {
