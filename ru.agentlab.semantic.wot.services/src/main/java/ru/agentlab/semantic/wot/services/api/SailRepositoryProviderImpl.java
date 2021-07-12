7e6aebfb2135675472de6bc9378d7ec157763eca
Dnnd <d.v.maslennikov@outlook.com>
2021-07-12 17:12:36 +0300

    externalize repository configuration

--- ru.agentlab.semantic.wot.services/src/main/java/ru/agentlab/semantic/wot/services/api/SailRepositoryProviderImpl.java
+++ ru.agentlab.semantic.wot.services/src/main/java/ru/agentlab/semantic/wot/services/api/SailRepositoryProviderImpl.java
@@ -1,21 +1,12 @@
 package ru.agentlab.semantic.wot.services.api;
 
-import org.eclipse.rdf4j.repository.config.RepositoryConfig;
 import org.eclipse.rdf4j.repository.manager.LocalRepositoryManager;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
-import org.eclipse.rdf4j.repository.sail.config.SailRepositoryConfig;
-import org.eclipse.rdf4j.sail.nativerdf.config.NativeStoreConfig;
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.ConfigurationPolicy;
-import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.*;
 import org.osgi.service.metatype.annotations.Designate;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
-import ru.agentlab.changetracking.sail.ChangeTrackerConfig;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 
 @Component(configurationPolicy = ConfigurationPolicy.REQUIRE, service = SailRepositoryProvider.class)
 @Designate(ocd = SailRepositoryProviderImpl.Config.class)
@@ -25,24 +16,16 @@ public class SailRepositoryProviderImpl implements SailRepositoryProvider {
 
     @ObjectClassDefinition
     @interface Config {
-        String baseDir();
-
         String repoID();
+    }
+
+    @Reference
+    public void bindLocalRepositoryManager(LocalRepositoryManager manager) {
+        this.repositoryManager = manager;
     }
 
     @Activate
     public void activate(Config config) throws IOException {
-        File baseDir = new File(config.baseDir());
-        Files.createDirectories(baseDir.toPath());
-
-        repositoryManager = new LocalRepositoryManager(baseDir);
-        repositoryManager.init();
-
-        ChangeTrackerConfig trackerConfig = new ChangeTrackerConfig(new NativeStoreConfig());
-        repositoryManager.addRepositoryConfig(new RepositoryConfig(
-                config.repoID(),
-                new SailRepositoryConfig(trackerConfig)
-        ));
         repo = (SailRepository) repositoryManager.getRepository(config.repoID());
     }
 
